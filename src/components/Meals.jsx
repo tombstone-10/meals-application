8bc4881d35455dd8eef6d91a15e768a0e32f3f4b
Ali Ahmed <tombstone.act@gmail.com>
2024-06-18 04:22:17 +0500

    UseContext Custom Hook

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,6 +1,9 @@
 import "../App.css";
+import { useGlobalContext } from "../context";
 
 const Meals = () => {
+  const context = useGlobalContext();
+  console.log(context);
   return <h1>Meals</h1>;
 };
 
--- src/context.jsx
+++ src/context.jsx
@@ -3,7 +3,15 @@ import React, { useContext } from "react";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  return (
+    <AppContext.Provider value={{ name: "ali", role: "student" }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
 };
 
 export { AppContext, AppProvider };

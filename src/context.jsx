69eeb87924ac124c880bf36f37d49252cc87c0cd
Ali Ahmed <tombstone.act@gmail.com>
2024-06-25 04:57:21 +0500

    Update context.jsx

--- src/context.jsx
+++ src/context.jsx
@@ -1,8 +1,24 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 
 const AppContext = React.createContext();
 
+const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=a";
+const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
+
 const AppProvider = ({ children }) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("https://randomuser.me/api/");
+        const data = await res.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <AppContext.Provider value={{ name: "ali", role: "student" }}>
       {children}
